2c3faa9cb720b01946f8115754ef69cbe2c00c4f
Tom Armitage <tom@infovore.org>
2010-01-04 21:48:05 +0000

    Min/max scores.

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -4,4 +4,12 @@ class Writer < ActiveRecord::Base
   def average_score
     Review.average(:score, :conditions => {:writer_id => self})
   end
+  
+  def min_score
+    Review.minimum(:score, :conditions => {:writer_id => self})
+  end
+  
+  def max_score
+    Review.maximum(:score, :conditions => {:writer_id => self})
+  end
 end
