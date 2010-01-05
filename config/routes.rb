5636a5ab90ffc093d73af4f3ae9bb57ae98afb3d
Tom Armitage <tom@infovore.org>
2010-01-05 10:11:28 +0000

    Home route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :reviews
   map.resources :writers
   map.resources :scores
+  
+  map.root :controller => "reviews", :action => "index"
 end
