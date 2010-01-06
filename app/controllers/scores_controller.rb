d71f09fcdaa8daa144ebac2d9f1bb14a7bfdd208
Tom Armitage <tom@infovore.org>
2010-01-06 14:34:27 +0000

    Titles on pages, scores index, all sorts of visual tweaks.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,4 +1,12 @@
 class ScoresController < ApplicationController
+  
+  def index
+    @counts = []
+    (1..10).each do |i|
+      @counts << Review.count(:all, :conditions => {:score => i})
+    end
+  end
+  
   def show
     @score = params[:id]
     @reviews = Review.all(:conditions => {:score => @score})
