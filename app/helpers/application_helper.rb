ef966835462f1d22ebbb558b98bfb9de807038a6
Tom Armitage <tom@infovore.org>
2010-01-06 22:46:25 +0000

    Heck yes typekit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def cleardiv
+    "<span class='cleardiv'><!-- clear --></span>"
+  end
+  
 end
