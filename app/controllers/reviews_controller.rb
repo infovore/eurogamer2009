79999ed90a7cd2bd32e747e201855756daf84338
Tom Armitage <tom@infovore.org>
2010-01-09 12:30:44 +0000

    Method to update unscored reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,14 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
   end
   
+  def unscored
+    @reviews = Review.find(:all, :conditions => {:score => nil})
+  end
+  
+  def update
+    @review = Review.find(params[:id])
+    @review.score = params[:review][:score]
+    @review.save
+    redirect_to unscored_reviews_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :reviews
+  map.resources :reviews, :collection => {:unscored => :get}
   map.resources :writers
   map.resources :scores
   
